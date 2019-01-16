986f9d94f93af65a871ff95eacb7cbe24d4aba23
Ryan-Houpt <rehoupt@gmail.com>
2019-01-16 13:58:14 -0600

    first commit

---
+++ src/main/java/com/codeup/pettential/PettentialApplication.java
@@ -0,0 +1,14 @@
+package com.codeup.pettential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettentialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PettentialApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/codeup/pettential/PettentialApplicationTests.java
@@ -0,0 +1,17 @@
+package com.codeup.pettential;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PettentialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
