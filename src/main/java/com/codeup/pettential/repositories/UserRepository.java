4f7c54c3e2eeb16e7272c65e22ce5fb4fd9f2611
Ryan-Houpt <rehoupt@gmail.com>
2019-01-17 15:07:49 -0600

    shelter controller update

--- src/main/java/com/codeup/pettential/repositories/UserRepository.java
+++ src/main/java/com/codeup/pettential/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package com.codeup.pettential.repositories;
 import com.codeup.pettential.models.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Long> {
 }
