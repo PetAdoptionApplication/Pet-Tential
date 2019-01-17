dc1a9708209fc0c51adc008f0fa823915b253f16
Ryan-Houpt <rehoupt@gmail.com>
2019-01-17 12:14:38 -0600

    preferences page and controller

---
+++ src/main/java/com/codeup/pettential/repositories/PreferencesRepository.java
@@ -0,0 +1,10 @@
+package com.codeup.pettential.repositories;
+
+
+import com.codeup.pettential.models.Preferences;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PreferencesRepository extends CrudRepository <Preferences, Long> {
+
+
+}
