d807c53f091bde59fead92e7d6385d136b5cd931
Kyla Allen <themightyflorp@Kylas-MacBook-Air.local>
2019-01-24 10:39:41 -0600

    added the configure method to the authentication  controller

--- src/main/java/com/codeup/pettential/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/pettential/controllers/AuthenticationController.java
@@ -1,12 +1,24 @@
 package com.codeup.pettential.controllers;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class AuthenticationController {
     @GetMapping("/login")
     public String showLoginForm() {
         return "system/log-in";
+    }
+
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                .formLogin()
+                .loginPage("/system/login.html")
+                .failureUrl("/login-error.html")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/system/login.html");
     }
 }
