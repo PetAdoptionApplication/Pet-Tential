af3b0f72c3ddeefa95338dd5242a40b266ebc559
KennethMcInturf <kennethcmcinturf@gmail.com>
2019-01-29 11:30:22 -0600

    fixed profile

--- seeder.sql
+++ seeder.sql
@@ -5,7 +5,7 @@ values ('KC', '325-998-4721', '$2a$10$2C0uxzhqZpk8doeIi2fH/eIfj8Xt6.t9RPd9LZRdaJ
 ('Shelter', '325-998-4721', '$2a$10$2C0uxzhqZpk8doeIi2fH/eIfj8Xt6.t9RPd9LZRdaJ6nWK64MA8ma', 'shelter', '302 Chicago Blvd, San Antonio, Texas', 'shelter@email.com', 1);
 
 INSERT into shelter (name, number, location, email, user_id, number_of_pets)
-values ('KC', '325-998-4721', '302 Chicago Blvd, San Antonio, Texas', 'kc@email.com', 3, 3);
+values ('The Good Place', '325-998-4721', '302 Chicago Blvd, San Antonio, Texas', 'kc@email.com', 3, 3);
 
 INSERT INTO pet (age, breed, color, description, name, picture, sex, weight, shelter_id)
 VALUES (3, 'German Shepherd', 'brown/black', 'great with kids', 'Axel', 'https://localnewsonly.com/wp-content/uploads/2018/12/dog-adoption-1.jpg', 'Male', 45, 1),
